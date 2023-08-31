0240f841941e3de26c298d3218ff6da65de23737
21f3000984 <143736302+21f3000984@users.noreply.github.com>
2023-08-31 21:52:49 +0530

    Update shrodinger.py

--- shrodinger.py
+++ shrodinger.py
@@ -14,5 +14,5 @@ def main():
     result = largest_number(a, b, c)
     st.write(f"The largest number among {a}, {b}, and {c} is: {result}")
 
-if name == "main":
+if __name__ == "__main__":
     main()
